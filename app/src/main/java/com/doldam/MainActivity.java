534a591b527fcb30f3cb5907ff533c2a743a1c2c
Hanul <nul010@naver.com>
2017-07-25 17:53:53 +0900

    select active

--- app/src/main/java/com/doldam/MainActivity.java
+++ app/src/main/java/com/doldam/MainActivity.java
@@ -1,8 +1,10 @@
 package com.doldam;
 
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -24,11 +26,40 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
         tabLayout.getTabAt(0).setIcon(R.drawable.timeline);
         tabLayout.getTabAt(1).setIcon(R.drawable.star);
         tabLayout.getTabAt(2).setIcon(R.drawable.heart);
         tabLayout.getTabAt(3).setIcon(R.drawable.profile);
 
+        TabLayout.Tab tab = tabLayout.getTabAt(0);
+        int tabIconColor = ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary);
+        tab.getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
+
+
+        tabLayout.setOnTabSelectedListener(
+                new TabLayout.ViewPagerOnTabSelectedListener(viewPager) {
+
+                    @Override
+                    public void onTabSelected(TabLayout.Tab tab) {
+                        super.onTabSelected(tab);
+                        int tabIconColor = ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary);
+                        tab.getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
+                    }
+
+                    @Override
+                    public void onTabUnselected(TabLayout.Tab tab) {
+                        super.onTabUnselected(tab);
+                        int tabIconColor = ContextCompat.getColor(getApplicationContext(), R.color.inActive);
+                        tab.getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
+                    }
+
+                    @Override
+                    public void onTabReselected(TabLayout.Tab tab) {
+                        super.onTabReselected(tab);
+                    }
+                }
+        );
 
 
     }
